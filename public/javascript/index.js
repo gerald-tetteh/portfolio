665ab047d62a234b6a8d08f77a44ad1e4862961f
Gerald Addo-Tetteh <geraldadt@outlook.com>
2020-05-19 10:23:00 +0000

    initial commit converted to express app

---
+++ public/javascript/index.js
@@ -0,0 +1,20 @@
+const nav = document.querySelector(".nav-wrapper");
+
+$(document).ready(()=>{
+  $(".sidenav").sidenav();
+  $('.materialboxed').materialbox();
+  $(".parallax").parallax();
+  $('.collapsible').collapsible();
+  $('.tabs').tabs();
+});
+
+window.addEventListener("scroll",()=>{
+  if(window.pageYOffset > 0 ){
+    nav.classList.add("dark");
+    nav.classList.remove("transparent");
+  }
+  if(window.pageYOffset === 0){
+    nav.classList.remove("dark");
+    nav.classList.add("transparent")
+  }
+});
