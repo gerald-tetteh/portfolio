778a6eecf46c31dec68315635b2096db96dbc347
Gerald Addo-Tetteh <geraldadt@outlook.com>
2020-06-07 13:22:12 +0000

    corrected text errors added error page

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const app = express();
 
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
   res.render("index");
 });
 
+app.get("*", (req, res) => {
+  res.render("error_page");
+});
+
 const port = process.env.PORT || 3000
-app.listen(port,()=>{
+app.listen(port, () => {
   console.log("Server Running");
 });
