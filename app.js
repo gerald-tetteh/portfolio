6d08c274143f0d0de64d0eed138277962f3297d0
Gerald Addo-Tetteh <geraldadt@outlook.com>
2020-09-05 16:09:16 +0000

    added stereobeats route, completed header block(stereo), changed file system

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
+app.get("/stereobeats", (req, res) => {
+  res.render("projects/stereo_beats");
+});
+
 app.get("*", (req, res) => {
   res.render("error_page");
 });
