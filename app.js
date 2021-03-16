33017ccdd3767ef860ad6c80497f44114d70d140
Gerald Addo-Tetteh <geraldadt@outlook.com>
2021-03-16 03:46:45 +0000

    completed home page redesign and added route to privacy policy

--- app.js
+++ app.js
@@ -5,10 +5,6 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("index");
-});
-
-app.get("/home", (req, res) => {
   res.render("home/home");
 });
 
@@ -16,11 +12,15 @@ app.get("/stereobeats", (req, res) => {
   res.render("projects/stereo_beats");
 });
 
+app.get("/qr-scanner/privacy-policy", (req, res) => {
+  res.render("privacy");
+});
+
 app.get("*", (req, res) => {
   res.render("error_page");
 });
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log("Server Running");
 });
