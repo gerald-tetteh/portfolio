4ba5ab2671fd045cb7d0721915b24b384ace3c4b
Gerald Addo-Tetteh <geraldadt@outlook.com>
2022-07-26 18:52:10 +0000

    complete landing page

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,18 +1,51 @@
 // HomePage
 
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import Sidebar from "../components/home-page/Sidebar";
 import { useInView } from "react-intersection-observer";
-import geraldImg from "../images/gerald.png";
+import flutterIcon from "../images/flutter.svg";
+import nodeIcon from "../images/node.svg";
+import pythonIcon from "../images/python.svg";
+import androidIcon from "../images/android.svg";
 
 const HomePage = () => {
   const observerOptions = {
     threshold: 1,
   };
-  const { ref: section1, inView: s1InView } = useInView(observerOptions);
+  const headingRef = useRef();
   const { ref: section2, inView: s2InView } = useInView(observerOptions);
   const { ref: section3, inView: s3InView } = useInView(observerOptions);
-  useEffect(() => {}, []);
+  const headingText = "GERALD ADDO-TETTEH";
+  let headingAnimationIdx = 0;
+  let headingTextInterval;
+  const createHeadingSpan = () => {
+    while (headingAnimationIdx !== headingText.length) {
+      let text = headingText[headingAnimationIdx];
+      if (text === " ") {
+        text = "&nbsp;";
+      }
+      const element = `<span>${text}</span>`;
+      headingRef.current.innerHTML += element;
+      headingAnimationIdx++;
+    }
+  };
+  const handleTitleAnimation = () => {
+    if (headingAnimationIdx === headingText.length) {
+      return clearInterval(headingTextInterval);
+    }
+    headingRef.current.children[headingAnimationIdx].classList.add(
+      "animate-letter"
+    );
+    headingAnimationIdx++;
+  };
+  useEffect(() => {
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    createHeadingSpan();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    headingAnimationIdx = 0;
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    headingTextInterval = setInterval(handleTitleAnimation, 90);
+  }, []);
   return (
     <div className="home-page">
       <Sidebar />
@@ -36,15 +69,41 @@ const HomePage = () => {
         </ul>
       </nav>
       <main className="home-page__main">
-        <section
-          ref={section1}
-          className="home-page__section home-page__section-1"
-        >
-          <img
-            class="home-page__section-1__image"
-            src={geraldImg}
-            alt="Gerald Addo-Tetteh"
-          />
+        <section className="home-page__section home-page__section-1">
+          <div className="home-page__section-1__content">
+            <div className="home-page__section-1__text">
+              <h1 ref={headingRef} className="home-page__section-1__heading">
+                {}
+              </h1>
+              <h2 className="home-page__section-1__sub-heading">
+                Mobile and Web Application Developer with Skills in Electrical
+                and Electronic Engineering.
+              </h2>
+            </div>
+            <div className="home-page__section-1__bar"></div>
+          </div>
+          <div className="home-page__section-1__icons">
+            <img
+              src={flutterIcon}
+              alt="Flutter icon"
+              className="home-page__section-1__icon"
+            />
+            <img
+              src={pythonIcon}
+              alt="Python icon"
+              className="home-page__section-1__icon"
+            />
+            <img
+              src={nodeIcon}
+              alt="Node js icon"
+              className="home-page__section-1__icon"
+            />
+            <img
+              src={androidIcon}
+              alt="Android icon"
+              className="home-page__section-1__icon"
+            />
+          </div>
         </section>
         <section
           ref={section2}
